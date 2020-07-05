69970aeafbd0e4d690857c5f34179b4a0d7a6d5a
teebbstudios <qww.zone@gmail.com>
2020-07-05 23:22:02 +0800

    add fos js routing

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
 ];
